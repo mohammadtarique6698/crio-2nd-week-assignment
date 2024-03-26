aab973a309f6604bb4f7bb87d4410ed78741fe04
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2024-03-26 23:20:54 +0530

    Change in vite configuration.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1024 * 500, // Adjust the limit as needed, this example sets it to 500 kB
+  },
+});
