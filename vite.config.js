1e4e416dc0002209b9d136390a423825bf12f7aa
mohammadtarique6698 <mohammadtarique661998@gmail.com>
2024-03-26 23:37:32 +0530

    Removed the added configuration from vite.config.js for testing.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    chunkSizeWarningLimit: 1024 * 500, // Adjust the limit as needed, this example sets it to 500 kB
-  },
 });
